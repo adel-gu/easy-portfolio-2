27ac27b4c0b442f93f48f2f1e1ab616515e139bc
adel <adeelguitoun@gmail.com>
2024-08-13 08:25:11 +0100

    Customize font-family, background and foreground colors

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,12 @@
 import type { Metadata } from 'next';
 import { Space_Grotesk } from 'next/font/google';
 import './globals.css';
+import { cn } from '@/lib/utils';
 
-const spaceGrotesk = Space_Grotesk({ subsets: ['latin'] });
+const spaceGrotesk = Space_Grotesk({
+  subsets: ['latin'],
+  variable: '--space-grotesk',
+});
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -16,7 +20,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={spaceGrotesk.className}>{children}</body>
+      <body
+        className={cn(
+          'min-h-screen bg-background font-space-grotesk antialiased',
+          spaceGrotesk.variable,
+        )}
+      >
+        {children}
+      </body>
     </html>
   );
 }
