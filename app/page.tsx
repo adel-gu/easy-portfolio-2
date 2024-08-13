918e404bd7d98c1001b36b2adcd2b7bd3c297a67
adel <adeelguitoun@gmail.com>
2024-08-13 22:38:32 +0100

    Create  hero section

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,8 @@ import Hero from '@/components/Hero';
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center justify-between">
+      <div className="w-[1200px] h-[600px] bg-gradient-to-b from-white to-black blur-[150px] absolute inset-x-[15%] top-[-65%] rounded-full z-[-5]" />
       <Hero />
     </main>
   );
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,4 +1,68 @@
+import Link from 'next/link';
+import { Dock, DockIcon } from '@/components/magicui/dock';
+import AnimatedGridPattern from '@/components/magicui/animated-grid-pattern';
+import { Github, Linkedin, Mail, Twitter } from 'lucide-react';
+
 const Hero = () => {
-  return <div>DHero</div>;
+  return (
+    <section className="relative h-screen w-full overflow-hidden">
+      <div className="container mx-auto h-full">
+        <div className="text-center h-full flex flex-col items-center justify-center gap-12">
+          <h1 className="pointer-events-none whitespace-pre-wrap bg-gradient-to-b bg-clip-text text-center text-8xl font-medium leading-none text-transparent from-slate-200 to-slate-600">
+            Crafting Memorable <br />
+            User Experiences
+            <br />
+            for Business Success
+          </h1>
+
+          <div className="relative">
+            <Dock magnification={60} distance={100}>
+              {socialLinks.map((socialLink) => (
+                <DockIcon className="bg-slate-200/10">
+                  <Link key={socialLink.name} href={socialLink.path}>
+                    {socialLink.icon}
+                  </Link>
+                </DockIcon>
+              ))}
+            </Dock>
+          </div>
+        </div>
+      </div>
+
+      <div className="absolute z-[-10] top-0 h-full w-full overflow-hidden [mask-image:radial-gradient(900px_circle_at_top,white,transparent)] opacity-50">
+        <AnimatedGridPattern
+          numSquares={100}
+          maxOpacity={0.2}
+          duration={5}
+          repeatDelay={1}
+          className="h-[150%] inset-y-[-30%]"
+        />
+      </div>
+    </section>
+  );
 };
+
+const socialLinks = [
+  {
+    name: 'linkedin',
+    icon: <Linkedin />,
+    path: '#',
+  },
+  {
+    name: 'github',
+    icon: <Github />,
+    path: '#',
+  },
+  {
+    name: 'twitter',
+    icon: <Twitter />,
+    path: '#',
+  },
+  {
+    name: 'email',
+    icon: <Mail />,
+    path: '#',
+  },
+];
+
 export default Hero;
